0e09c50f84a565be3203f0ff6651e768db9fa56f
Sveske_Juice <carl.benjamin.dreyer@gmail.com>
2025-03-18 11:46:06 +0100

    fix rebuilding error lateralus

--- home/common/optional/desktops/hyprland/exec.nix
+++ home/common/optional/desktops/hyprland/exec.nix
@@ -1,13 +1,13 @@
-{...}: {
+{pkgs, ...}: {
   wayland.windowManager.hyprland.settings.exec = [
-    "pkill -f hyprpaper;hyprpaper & disown"
+    "pkill -f ${pkgs.hyprpaper};${pkgs.hyprpaper} & disown"
   ];
   wayland.windowManager.hyprland.settings.exec-once = [
     # When managed by UWSM we use systemd
     "systemctl --user start hyprpolkitagent"
 
-    "waybar 2>&1 > /tmp/waybarlog.txt"
-    "syncthingtray --wait & disown"
-    "wl-paste --watch cliphist store & disown"
+    "${pkgs.waybar} 2>&1 > /tmp/waybarlog.txt"
+    "${pkgs.syncthingtray} --wait & disown"
+    "wl-paste --watch ${pkgs.cliphist} store & disown"
   ];
 }
