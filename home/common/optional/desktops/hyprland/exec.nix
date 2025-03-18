ed3505aa0c4888b4719d77fc6f5814790f18619b
Sveske-Juice <carl.benjamin.dreyer@gmail.com>
2025-03-18 12:31:29 +0100

    fix hyprland xdg stuff
    waybar not launching

--- home/common/optional/desktops/hyprland/exec.nix
+++ home/common/optional/desktops/hyprland/exec.nix
@@ -1,13 +1,13 @@
 {pkgs, ...}: {
   wayland.windowManager.hyprland.settings.exec = [
-    "pkill -f ${pkgs.hyprpaper};${pkgs.hyprpaper} & disown"
+    "pkill -f ${pkgs.hyprpaper}/bin/hyprpaper;${pkgs.hyprpaper}/bin/hyprpaper & disown"
   ];
   wayland.windowManager.hyprland.settings.exec-once = [
     # When managed by UWSM we use systemd
     "systemctl --user start hyprpolkitagent"
 
-    "${pkgs.waybar} 2>&1 > /tmp/waybarlog.txt"
-    "${pkgs.syncthingtray} --wait & disown"
-    "wl-paste --watch ${pkgs.cliphist} store & disown"
+    "${pkgs.waybar}/bin/waybar 2>&1 > /tmp/waybarlog.txt"
+    "${pkgs.syncthingtray}/bin/syncthingtray --wait & disown"
+    "wl-paste --watch ${pkgs.cliphist}/bin/cliphist store & disown"
   ];
 }
