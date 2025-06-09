8409d568802ec7c4d13afbb4574589606aba2afa
Sveske-Juice <carl.benjamin.dreyer@gmail.com>
2025-06-09 18:12:58 +0200

    switch formatter: nixfmt-treefmt

--- flake.nix
+++ flake.nix
@@ -5,6 +5,11 @@
     # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs.url = "github:Sveske-Juice/nixpkgs/update-syncthing";
 
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -56,48 +61,58 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  } @ inputs: let
-    inherit (nixpkgs) lib;
-    inherit (inputs) nvf;
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system;};
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+      ...
+    }@inputs:
+    let
+      inherit (nixpkgs) lib;
+      inherit (inputs) nvf;
 
-    # Create a nixos configuration for specified host
-    mkHost = host: {
-      ${host} = lib.nixosSystem {
-        specialArgs = {
-          inherit self;
-          inherit inputs;
+      # Create a nixos configuration for specified host
+      mkHost = host: {
+        ${host} = lib.nixosSystem {
+          specialArgs = {
+            inherit self;
+            inherit inputs;
+          };
+          modules = [ ./hosts/nixos/${host} ];
         };
-        modules = [./hosts/nixos/${host}];
       };
+
+      # Small tool to iterate over each systems
+      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+
+      # Eval the treefmt modules from ./treefmt.nix
+      treefmtEval = eachSystem (pkgs: inputs.treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
+
+      # Invoke mkHost foreach host config in passed hosts param
+      # foldl for left folding which combines all the hosts into 1 attribute-set
+      mkHostConfigs = hosts: lib.foldl (a: b: a // b) { } (lib.map (host: mkHost host) hosts);
+
+      readHosts = folder: lib.attrNames (builtins.readDir ./hosts/${folder});
+    in
+    {
+      # nix fmt
+      formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
+      # nix flake check
+      checks = eachSystem (pkgs: {
+        formatting = treefmtEval.${pkgs.system}.config.build.check self;
+      });
+
+      # Custom packages
+      packages = eachSystem {
+        "neovim" =
+          (nvf.lib.neovimConfiguration {
+            pkgs = nixpkgs.legacyPackages.x86_64-linux;
+            modules = [ ./modules/nvf ];
+          }).neovim;
+      };
+
+      # Automatically generate nixos configurations foreach sub directory under ./hosts
+      nixosConfigurations = mkHostConfigs (readHosts "nixos");
     };
-
-    # Invoke mkHost foreach host config in passed hosts param
-    # foldl for left folding which combines all the hosts into 1 attribute-set
-    mkHostConfigs = hosts: lib.foldl (a: b: a // b) {} (lib.map (host: mkHost host) hosts);
-
-    readHosts = folder: lib.attrNames (builtins.readDir ./hosts/${folder});
-  in {
-    formatter.${system} = pkgs.alejandra;
-
-    # Custom packages
-    packages.${system} = {
-      "neovim" =
-        (nvf.lib.neovimConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          modules = [
-            ./modules/nvf
-          ];
-        })
-        .neovim;
-    };
-
-    # Automatically generate nixos configurations foreach sub directory under ./hosts
-    nixosConfigurations = mkHostConfigs (readHosts "nixos");
-  };
 }
