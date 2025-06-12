8788240e6a994eaebbfa821827fefe66162877ff
Sveske-Juice <carl.benjamin.dreyer@gmail.com>
2025-06-12 11:45:47 +0200

    feat(waltherbox): ollama proxy

--- hosts/common/optional/services/nginx.nix
+++ hosts/common/optional/services/nginx.nix
@@ -1,6 +1,10 @@
-{ ... }:
+{ config, ... }:
 {
   users.groups.www = { };
+  sops.secrets.ollama-auth = {
+    owner = config.services.nginx.user;
+    group = config.services.nginx.group;
+  };
 
   services.nginx = {
     enable = true;
@@ -10,6 +14,18 @@
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
     statusPage = true;
+  };
+
+  services.nginx.virtualHosts."ollama.deprived.dev" = {
+    forceSSL = true;
+    enableACME = true;
+    http2 = true;
+    locations = {
+      "/" = {
+        basicAuthFile = config.sops.secrets.ollama-auth.path;
+        proxyPass = "http://192.168.1.70:11434";
+      };
+    };
   };
 
   security.acme.acceptTerms = true;
