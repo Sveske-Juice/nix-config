fd1ce9430f4db5adf6fd24bbb9696e35d019359a
Sveske-Juice <carl.benjamin.dreyer@gmail.com>
2025-06-28 12:48:55 +0200

    feat(waltherbox): jellyseerr nginx

--- hosts/common/optional/services/jellyseerr.nix
+++ hosts/common/optional/services/jellyseerr.nix
@@ -1,7 +1,17 @@
-{ ... }:
+{ config, ... }:
 {
   services.jellyseerr = {
     enable = true;
     openFirewall = true;
+  };
+
+  services.nginx.virtualHosts."jellyseerr.casdnas.deprived.dev" = {
+    addSSL = true;
+    enableACME = true;
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:${toString config.services.jellyseerr.port}";
+      proxyWebsockets = true;
+      recommendedProxySettings = true;
+    };
   };
 }
