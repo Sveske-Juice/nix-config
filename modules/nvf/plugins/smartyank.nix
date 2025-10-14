14fbcad45db3cdbeb6fcaf12dbb6268a08873dd0
Sveske-Juice <carl.benjamin.dreyer@gmail.com>
2025-10-14 14:29:44 +0200

    feat: nvim nvf osc 52 with smartyank

--- modules/nvf/plugins/smartyank.nix
+++ modules/nvf/plugins/smartyank.nix
@@ -4,5 +4,7 @@
     package = pkgs.vimPlugins.smartyank-nvim;
     setupModule = "smartyank";
     setupOpts.osc52.ssh_only = false;
+    setupOpts.tmux.enabled = false; # use local tmux on host
+    # setupOpts.tmux.cmd = [ "tmux" "set-buffer" "-w" ];
   };
 }
