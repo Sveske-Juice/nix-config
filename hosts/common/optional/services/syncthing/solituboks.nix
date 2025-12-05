ee220fd6d127e35683e79e763c8a913534748796
Sveske-Juice <carl.benjamin.dreyer@gmail.com>
2025-12-05 20:57:54 +0100

    fix(syncthing): cfg on solituboks

--- hosts/common/optional/services/syncthing/solituboks.nix
+++ hosts/common/optional/services/syncthing/solituboks.nix
@@ -1,17 +1,10 @@
 { config, ... }:
 {
   imports = [ ./core.nix ];
-  system.activationScripts.createsyncdir = ''
-    install -Dm 770 -o ${config.services.syncthing.user} -g ${config.services.syncthing.group} /sync
-  '';
 
   services.syncthing = {
-    dataDir = "/sync";
-    user = "syncthing";
-    group = "data";
     settings = {
       gui = {
-        user = config.hostSpec.username;
         password = "$2b$05$I0ofnse7HEEVqyvgjwD3FOLGiXHbobSUURvud3iR3z6LKi461puyS";
       };
     };
