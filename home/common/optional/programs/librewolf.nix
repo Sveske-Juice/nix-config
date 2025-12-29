bb00194e1363549f5f956f59f6d7c4cad880a614
Sveske-Juice <carl.benjamin.dreyer@gmail.com>
2025-12-29 11:15:29 +0100

    fix: librewolf not working on new monitor

--- home/common/optional/programs/librewolf.nix
+++ home/common/optional/programs/librewolf.nix
@@ -2,7 +2,7 @@
 {
   programs.firefox = {
     enable = true;
-    package = pkgs.librewolf;
+    # package = pkgs.librewolf;
     policies = {
       DisableTelemetry = true;
       DisableFirefoxStudies = true;
