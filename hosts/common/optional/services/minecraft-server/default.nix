6970228d47a0778683a23357cb74ada62ae88438
Sveske-Juice <carl.benjamin.dreyer@gmail.com>
2025-12-30 00:29:11 +0100

    feat(mc): extra vanilla server

--- hosts/common/optional/services/minecraft-server/default.nix
+++ hosts/common/optional/services/minecraft-server/default.nix
@@ -1,5 +1,8 @@
 {inputs, pkgs, config, ...}: let
-  rcon-port = 25575;
+  cvpl-port = 25565;
+  cvpl-rcon-port = 25575;
+  alexworld-port = 25566;
+  alexworld-rcon-port = 25576;
 in {
   imports = [ inputs.nix-minecraft.nixosModules.minecraft-servers ];
   nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
@@ -10,6 +13,43 @@ in {
 
   sops.secrets."minecraft/mc-cvpl/ops/op1/name".owner = config.services.minecraft-servers.user;
   sops.secrets."minecraft/mc-cvpl/ops/op1/uuid".owner = config.services.minecraft-servers.user;
+
+  sops.secrets."minecraft/alex-world/rcon-password".owner = config.services.minecraft-servers.user;
+  sops.secrets."minecraft/alex-world/ops/op1/name".owner = config.services.minecraft-servers.user;
+  sops.secrets."minecraft/alex-world/ops/op1/uuid".owner = config.services.minecraft-servers.user;
+  sops.secrets."minecraft/alex-world/ops/op2/name".owner = config.services.minecraft-servers.user;
+  sops.secrets."minecraft/alex-world/ops/op2/uuid".owner = config.services.minecraft-servers.user;
+
+  # TODO: automate this
+  sops.templates."alex-world-ops" = {
+    owner = config.services.minecraft-servers.user;
+    content = ''
+      [
+        {
+          "uuid": "${config.sops.placeholder."minecraft/alex-world/ops/op1/uuid"}",
+          "name": "${config.sops.placeholder."minecraft/alex-world/ops/op1/name"}",
+          "level": 4,
+          "bypassesPlayerLimit": true
+        },
+        {
+          "uuid": "${config.sops.placeholder."minecraft/alex-world/ops/op2/uuid"}",
+          "name": "${config.sops.placeholder."minecraft/alex-world/ops/op2/name"}",
+          "level": 4,
+          "bypassesPlayerLimit": true
+        },
+      ]
+    '';
+  };
+
+  sops.templates."alex-world-server-properties" = {
+    owner = config.services.minecraft-servers.user;
+    content = ''
+      difficulty=hard
+      gamemode=survival
+      rcon.port=${toString alexworld-rcon-port}
+      rcon.password=${config.sops.placeholder."minecraft/alex-world/rcon-password"}
+    '';
+  };
 
   sops.templates."mc-cvpl-ops" = {
     owner = config.services.minecraft-servers.user;
@@ -24,6 +64,7 @@ in {
       ]
     '';
   };
+
   sops.templates."mc-cvpl-server-properties" = {
     owner = config.services.minecraft-servers.user;
     content = ''
@@ -65,10 +106,10 @@ in {
       pause-when-empty-seconds=60
       player-idle-timeout=0
       prevent-proxy-connections=false
-      query.port=25565
+      query.port=${toString cvpl-port}
       rate-limit=0
       rcon.password=${config.sops.placeholder."minecraft/mc-cvpl/rcon-password"}
-      rcon.port=${toString rcon-port}
+      rcon.port=${toString cvpl-rcon-port}
       region-file-compression=deflate
       require-resource-pack=false
       resource-pack=
@@ -76,7 +117,7 @@ in {
       resource-pack-prompt=
       resource-pack-sha1=
       server-ip=
-      server-port=25565
+      server-port=${toString cvpl-port}
       simulation-distance=10
       spawn-protection=16
       status-heartbeat-interval=0
@@ -89,7 +130,13 @@ in {
     '';
   };
 
-  networking.firewall.allowedTCPPorts = [ rcon-port ];
+  networking.firewall.allowedTCPPorts = [
+    cvpl-port
+    cvpl-rcon-port
+
+    alexworld-port
+    alexworld-rcon-port
+  ];
 
   services.minecraft-servers = {
     enable = true;
@@ -100,6 +147,8 @@ in {
       enable = true;
       jvmOpts = "-Xmx18G -Xms2G";
       package = pkgs.fabricServers.fabric-1_21_11;
+
+      serverProperties.server-port = cvpl-port;
 
       # mods
       symlinks = {
@@ -132,6 +181,44 @@ in {
             C2ME = pkgs.fetchurl {
               url = "https://cdn.modrinth.com/data/VSNURh3q/versions/lYSxkbzC/c2me-fabric-mc1.21.11-0.3.6%2Brc.1.0.jar";
               sha256 = "sha256-pcvM0y05Iul3OPrHHMZ9k4EDT5vwj/0B9gjEIA/R/b8=";
+            };
+            DH = pkgs.fetchurl {
+              url = "https://cdn.modrinth.com/data/uCdwusMi/versions/GT3Bm3GN/DistantHorizons-2.4.5-b-1.21.11-fabric-neoforge.jar";
+              sha256 = "sha256-dpTHoX5V9b7yG0VsIqKxxOSAYLN0Z97itx1MEuWGvD8=";
+            };
+          }
+        );
+      };
+    };
+
+    servers.alex-world = {
+      enable = true;
+      jvmOpts = "-Xmx6G -Xms1G";
+      package = pkgs.fabricServers.fabric-1_21_4;
+
+      serverProperties.server-port = alexworld-port;
+
+      symlinks = {
+        "server.properties" = config.sops.templates."alex-world-server-properties".path;
+        "ops.json" = config.sops.templates."alex-world-ops".path;
+
+        mods = pkgs.linkFarmFromDrvs "mods" (
+          builtins.attrValues {
+            Fabric-API = pkgs.fetchurl {
+              url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/p96k10UR/fabric-api-0.119.4%2B1.21.4.jar";
+              sha256 = "sha256-0YO6y4RRZ/CSZML5AyK37P/ogm3r2m9g5ZeIkmS+9K8=";
+            };
+            DH = pkgs.fetchurl {
+              url = "https://cdn.modrinth.com/data/uCdwusMi/versions/NhMXwQCV/DistantHorizons-2.4.5-b-1.21.4-fabric-neoforge.jar";
+              sha256 = "sha256-qk4c3J12Kb/+C1g0wiahEwhioVgLb4f1EI2tAQdhPX0=";
+            };
+            Chunky = pkgs.fetchurl {
+              url = "https://cdn.modrinth.com/data/fALzjamp/versions/VkAgASL1/Chunky-Fabric-1.4.27.jar";
+              sha256 = "sha256-A8kKcLIzQWvZZziUm+kJ0eytrHQ/fBVZQ18uQXN9Qf0=";
+            };
+            C2ME = pkgs.fetchurl {
+              url = "https://cdn.modrinth.com/data/VSNURh3q/versions/EzvMx6b2/c2me-fabric-mc1.21.4-0.3.1.3.0.jar";
+              sha256 = "sha256-i3r6jdtLW10kpZ70s+OvAxchpVdrRZ+6O06AvR99MZE=";
             };
           }
         );
